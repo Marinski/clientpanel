18dcda0802691750eb5ccdecf0757903a535560b
Marinski <marinski@home>
2017-09-02 20:40:24 +0300

    Add service, model and config files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+// AngularFire imports
+import { AngularFireModule } from 'angularfire2'
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+// Component imports
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ClientsComponent } from './components/clients/clients.component';
@@ -14,6 +18,10 @@ import { RegisterComponent } from './components/register/register.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { EditClientComponent } from './components/edit-client/edit-client.component';
 import { SettingsComponent } from './components/settings/settings.component';
+// Service imports
+import { ClientService } from './services/client.service';
+// Database import
+import { firebaseConfig } from '../../config/firebase-config';
 
 const appRoutes: Routes = [
   {path: '', component: DashboardComponent},
@@ -38,9 +46,14 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
-  providers: [],
+  providers: [
+    AngularFireAuth,
+    AngularFireDatabase,
+    ClientService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/client.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
+import { Observable } from 'rxjs';
+import { Client } from '../models/Client';
+
+@Injectable()
+export class ClientService {
+  clients: FirebaseListObservable<any[]>;
+  client: FirebaseObjectObservable<any>;
+
+  constructor(public afd: AngularFireDatabase) {
+    this.clients = this.afd.list('/clients') as FirebaseListObservable<Client[]>;
+  }
+
+  getClients(){
+    return this.clients;
+  }
+
+}
